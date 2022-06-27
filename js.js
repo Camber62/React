2b72beb8022feb5e5a71e29a0df5f67ed0455148
Camber62 <94636483+Camber62@users.noreply.github.com>
2022-06-27 20:41:48 +0300

    Update js.js

--- js.js
+++ js.js
@@ -79,3 +79,97 @@ reportWebVitals();
 // if(value){
 //     input.setAttribute('value', value)
 // }
+
+lesson2
+
+import React ,{useState}from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const initCount = 0
+
+
+root.render(
+    <div>
+        <FavoritesList/>
+    </div>
+);
+
+function FavoritesList(){
+    const [foods,setFoods] = useState(['Пльмени','Паста','Пицца','Окрошка']);
+function addFoot(){
+    const food = "Чипсы"
+    setFoods(foods.concat([food]))
+}
+
+    return(
+        <div>
+            <ul>
+                {foods.map(function (food) {
+                    return (<li>{food}</li>);
+                })}
+            </ul>
+            <button onClick={addFoot}>addFoot</button>
+        </div>
+    )
+}
+
+
+// root.render(
+//     <div class="red">
+//         <HelloWorld name="Alexey" lang="en"/>
+//         <HelloWorld name="Алексей" lang="ru"/>
+//     </div>
+// );
+//
+// function HelloWorld(Props) {
+//     Props.name='Petr'
+//     if (Props.lang==="ru"){
+//         return (
+//             <h1>Привет, {Props.name}</h1>
+//         )
+//     }
+//     if (Props.lang==="en"){
+//         return (
+//             <h1>Hello,{Props.name}</h1>
+//         )
+//     }
+// }
+
+
+// }
+// root.render(
+//     <Cont count="100"/>
+// )
+//
+// function Cont(Props){
+//     const [count,setCont] =useState(Props.count)
+//     function clickOne(i){
+//         setCont(count+i)
+//     }
+//     function clickTwo(i){
+//         setCont(count-i)
+//     }
+//
+//     return (
+//         <div>
+//             <div>Значение:{count}</div>
+//             <button onClick={()=>clickOne(1)}>+1</button>
+//             <button onClick={()=>clickTwo(2)}>-1</button>
+//         </div>
+//     )
+//
+// }
+
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
